c169d9b4147bdb940a1c697a0d9ee5e9d13b055d
Jrozo <Jrozo@DESKTOP-031E2FB>
2020-05-05 15:04:13 -0500

    Creacion de la clase nodo

---
+++ fila_banco/src/fila_banco/Nodo.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fila_banco;
+
+/**
+ *
+ * @author Jrozo
+ */
+public class Nodo {
+    private char tipo;
+    private int dato;
+    private Nodo sig;
+
+    public Nodo(char t, int d, Nodo sig) {
+        this.tipo = t;
+        this.dato = d;
+        this.sig = sig;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo sig) {
+        this.sig = sig;
+    }
+    
+    
+}
